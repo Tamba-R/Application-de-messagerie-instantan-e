f4c48d2fd323b5068505decad9e09b4c3146bd4b
Roger Tamba <rogertambad@gmail.com>
2026-04-09 15:45:05 +0200

    congiguration et implementation des entitées (User-Messages)

---
+++ src/main/java/com/example/chatappdemo/entity/Message.java
@@ -0,0 +1,25 @@
+package com.example.chatappdemo.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "messages")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
---
+++ src/main/java/com/example/chatappdemo/entity/User.java
@@ -0,0 +1,38 @@
+package com.example.chatappdemo.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String username;
+    private String avatarColor;
+    private LocalDateTime Lastseen;
+    private boolean isOnline;
+
+    @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Message> sentMessages = new ArrayList<>();
+
+    public User(String username, String avatarColor){
+        this.username = username;
+        this.avatarColor = avatarColor;
+    }
+}
